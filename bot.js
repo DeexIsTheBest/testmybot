7f39efe4f6e10953657e5030a3627b63e482accc
DeexIsTheBest <45711286+DeexIsTheBest@users.noreply.github.com>
2019-01-08 16:04:58 +0000

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,264 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+ 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', function(msg) {
+    const prefix = '$'
+    if(msg.content.startsWith (prefix  + 'server')) {
+      let embed = new Discord.RichEmbed()
+      .setColor('RANDOM')
+      .setThumbnail(msg.guild.iconURL)
+      .setTitle(`Showing Details Of  **${msg.guild.name}*`)
+      .addField('🌐** نوع السيرفر**',`[** __${msg.guild.region}__ **]`,true)
+      .addField('🏅** __الرتب__**',`[** __${msg.guild.roles.size}__ **]`,true)
+      .addField('🔴**__ عدد الاعضاء__**',`[** __${msg.guild.memberCount}__ **]`,true)
+      .addField('🔵**__ عدد الاعضاء الاونلاين__**',`[** __${msg.guild.members.filter(m=>m.presence.status == 'online').size}__ **]`,true)
+      .addField('📝**__ الرومات الكتابية__**',`[** __${msg.guild.channels.filter(m => m.type === 'text').size}__** ]`,true)
+      .addField('🎤**__ رومات الصوت__**',`[** __${msg.guild.channels.filter(m => m.type === 'voice').size}__ **]`,true)
+      .addField('👑**__ الأونـر__**',`**${msg.guild.owner}**`,true)
+      .addField('🆔**__ ايدي السيرفر__**',`**${msg.guild.id}**`,true)
+      .addField('📅**__ تم عمل السيرفر في__**',msg.guild.createdAt.toLocaleString())
+      msg.channel.send({embed:embed});
+    }
+  });
+  
+  
+  client.on('message', message => {
+if(message.content.startsWith(prefix + 'mute')){//Toxic Codes
+    let role = message.guild.roles.find(r => r.name === 'Muted');//Toxic Codes
+    if(!role) message.guild.createRole({name: 'Muted'});s
+     if(user.bot){
+        return message.channel.send(`I can't mute ${user} because he is a bot`);//Toxic Codes
+    }
+    if(user.hasPermission('ADMINISTRATOR')) {
+        return message.channel.send(`لا تقدر اعطاء  ${user} ميوت لأنه من اداره`);//Toxic Codes
+    }
+   
+    if(!user){
+        message.channel.send(`There's no person to mute tho`);
+    }
+    message.guild.channels.forEach(f => {//Toxic Codes
+        f.overwritePermissions(role, {//Toxic Codes
+            SEND_MESSAGES: false
+        });
+        user.addRole(role);
+       
+    });
+     message.channel.send(`I muted ${user}`);
+}
+});//Toxic Codes
+ 
+ 
+client.on('message', message => {
+if(message.content.startsWith(prefix + 'unmute')){//Toxic Codes
+    let role = message.guild.roles.find(r => r.name === 'Muted');//Toxic Codes
+if(!user.roles.has(role)) {
+    return message.channel.send(`He is not muted`);//Toxic Codes
+}
+    user.removeRole(role).then(message.channel.send(`Unmuted ${user}`));
+   
+}
+}); 
+
+client.on('message', message => {
+    if (message.content.startsWith("رابط")) {
+ 
+  message.channel.createInvite({
+        thing: true,
+        maxUses: 100,
+        maxAge: 86400
+    }).then(invite =>
+      message.author.sendMessage(invite.url)
+    )
+    const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setDescription("| :white_check_mark:  | :heart:  تم ارسال الرابط على الخاص  ")
+      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
+              const Embed11 = new Discord.RichEmbed()
+        .setColor("RANDOM")
+                .setAuthor(message.guild.name, message.guild.iconURL)
+        .setDescription(`
+**
+---------------------
+-[${message.guild.name}]  هذا هو رابط سيرفر
+---------------------
+-هذا الرابط صالح ل 100 مستخدم فقط
+---------------------
+-هذا الرابط صالح لمده 24 ساعه فقط
+---------------------
+**`)
+      message.author.sendEmbed(Embed11)
+    }
+});
+
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(`:rose:  ولكم نورتنا :rose: 
+:crown:اسم العضو  ${member}:crown:  
+انت العضو رقم ${member.guild.memberCount} `) 
+}).catch(console.error)
+})
+
+client.on('message', message => {
+if (message.content.startsWith(prefix + 'help')) { //DiamondCodes - [ X_KillerYT ]
+    let pages = [`
+***__وصف عن البوت__***
+**
+:gem:  البوت فيه كثير ميزات حلوة و جميلة
+ ا:rocket: البوت يعمل قرابة 24 ساعة
+**
+        ***__General orders__***
+**
+『?serv /يعرض لك معلومات عن السيرفر』
+『?serv2 / يعرض لك معلومات عن السيرفر ( الكود الثاني ) للمعلومات』
+『?id / يعرض لك معلومات عنك』
+『?myroles / لرؤية جميع رتبك الشخصية بالسيرفر』
+『?id / يعرض لك معلومات عنك』
+『?link / لمعمل انفايت ( دعوة ) لشخص』
+『?inv / لدعوة البوت الى سيرفرك』
+『?support / سيرفر المساعدة』
+『?cmind / لكتابة اي شيء تقوله داخل صورة』
+『?servavatar / لرؤية صورة السيرفر』
+『?count / لرؤية عدد الاعضاء بالسيرفر』
+『?avatar / لرؤية صورة شخص 』
+『?bot-info / لرؤية معلومات عن البوت 』
+『?report / لرفع شكوى على عضو 』
+『?servers / لرؤية عدد السيرفرات التي داخل بها البوت 』
+『?myid / لمعرفة الايدي الخاص بك 』
+**
+  `
+,`
+        ***__Admin orders__***
+**
+『?clear / لحذف الشات 』
+『?mc / لقفل الشات  』
+『?unmc / لفتح الشات 』
+『?bc / لارسال رسالة لجميع اعضاء السيرفر 』
+『?kick / لطرد شخص من الدسكورد 』
+『?ban / لاعطاء شخص باند من الدسكورد 』
+『?mute / لاعطاء شخص ميوت 』
+『?unmute / لفك ميوت شخص 』
+『?ct / لانشاء روم كتابي 』
+『?cv / لانشاء روم صوتي 』
+『?rolebc / برود كاست للرتب 』
+**
+  `
+,`
+        ***__Games orders__***
+**
+『?لعبة صراحة / صراحة 』
+『?لعبة كت تويت / كت تويت 』
+『?لعبة لو خيروك / لو خيروك』
+『?rps / لعبة حجرة ورقة مقص 』
+『?اسئلة للعبة فورت نايت /  فورت نايت 』
+**
+   
+`]
+    let page = 1;
+ 
+    let embed = new Discord.RichEmbed()
+    .setColor('RANDOM')
+    .setFooter(`Page ${page} of ${pages.length}`)
+    .setDescription(pages[page-1])
+ 
+    message.author.sendEmbed(embed).then(msg => {
+ 
+        msg.react('◀').then( r => {
+            msg.react('▶')
+ 
+ 
+        const backwardsFilter = (reaction, user) => reaction.emoji.name === '◀' && user.id === message.author.id;
+        const forwardsFilter = (reaction, user) => reaction.emoji.name === '▶' && user.id === message.author.id;
+ 
+ 
+        const backwards = msg.createReactionCollector(backwardsFilter, { time: 2000000});
+        const forwards = msg.createReactionCollector(forwardsFilter, { time: 2000000});
+ 
+ 
+ 
+        backwards.on('collect', r => {
+            if (page === 1) return;
+            page--;
+            embed.setDescription(pages[page-1]);
+            embed.setFooter(`Page ${page} of ${pages.length}`);
+            msg.edit(embed)
+        })
+        forwards.on('collect', r => {
+            if (page === pages.length) return;
+     
+      page++;
+            embed.setDescription(pages[page-1]);
+            embed.setFooter(`Page ${page} of ${pages.length}`);
+            msg.edit(embed)
+        })
+        })
+    })
+    }
+});
+
+
+const secre = [
+  "**لو خيروك بين العيش وحدك في جزيرة كبيرة منعزلة مع أكبر درجات الرفاهية وبين العيش في مكان قديم ولكن مع أصدقائك المقربين**.",
+  "**لو خيروك بين فقدان ذاكرتك والعيش مع أصدقائك وأقربائك أو بقاء ذاكرتك ولكن العيش وحيد**.",
+  "**للو خيروك بين تناول الخضار والفاكهة طوال حياتك أو تناول اللحوم**.",
+  "**لو خيروك بين رؤية الأشباح فقط أو سماع صوتها فقط**.",
+  "**لو خيروك بين القدرة على سماع أفكار الناس أو القدرة على العودة في الزمن للخلف**.",
+  "**لو خيروك بين القدرة على الاختفاء أو القدرة على الطيران**.",
+  "**لو خيروك بين أن تعيش 5 دقائق في الماضي أو أن تعيشها في المستقبل**.",
+  "**لو خيروك بين 5 ملايين دولار أو 5 ملايين لحظة سعادة حقيقيةا**.",
+  "**لو خيروك بين الاعتذار عن خطأ اقترفته أو أن يقدم لك شخص أخطأ في حقق اعتذار**.",
+  "**لو خيروك بين الحقد أو المسامحة**.",
+  "**لو خيروك بين إنقاذ نفسك أو إنقاذ شخص وقد يعرضك ذلك للأذى**.",
+  "**لو خيروك بين أن تعيش في القرن الرابع عشر أو القرن الحالي**.",
+  "**لو خيروك بين امتلاك سرعة الفهد أو دهاء الثعلب**.",
+  "**لو خيروك بين أن تحصل على درجة كاملة في كامل اختباراتك القادمة والسابقة أو أن تسافر إلى بلد تحبه**.",
+  "**لو خيروك بين العيش بجانب الجبال والحدائق والأشجار أو العيش بجانب البحر**.",
+  "**لو خيروك بين تحقيق 3 أمنيات (لا تتعلق بأشخاص) أو اختيار 3 أشخاص للعيش معهم طوال حياتك**.",
+  "**لو خيروك بين النوم في غابة مظلمة أو على ظهر سفينة في يوم عاصف**.",
+  "**لو خيروك بين المال أو الجمال**.",
+  "**لو خيروك بين المال أو الذكاء**.",
+  "**لو خيروك بين المال أو الصحة**.",
+  "**لو خيروك بين الجمال أو الذكاء**.",
+  "**لو خيروك بين الذكاء أو الصحة**.",
+  "**لو خيروك بين إرسال رسالة صوتية لأمك مدة دقيقة كاملة لا تحتوي إلا على صراخك وخوفك، أو كسر بيضة نيئة على رأسك**.",
+]
+ 
+ 
+ client.on('message', message => {
+   if (message.content.startsWith("?لو خيروك")) {
+                if(!message.channel.guild) return message.reply('** This command only for servers**');
+  var embed = new Discord.RichEmbed()
+  .setColor('RANDOM')
+ 
+   .setThumbnail(message.author.avatarURL)
+ .addField('لعبه لو خيروك' ,
+  `${secre[Math.floor(Math.random() * secre.length)]}`)
+  message.channel.sendEmbed(embed);
+  console.log('[id] Send By: ' + message.author.username)
+    }
+});
+ 
+ 
+ client.on("message", message => {
+ 
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' ');
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {//حقوق دايموند كودز
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`);
+ message.delete();
+};    
+});
+ 
+ 
+ 
+ 
+ 
+ 
+client.login(process.env.BOT_TOKEN);
